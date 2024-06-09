48d9ddc70655ce28186159bd116cb2dc4e89c6d0
Fadhil <fadhilsaheer@gmail.com>
2024-06-09 22:57:26 +0530

    init: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import { Button } from './components/ui/button';
 
 export default function App() {
   return (
-    <section className='w-screen h-screen bg-black p-5'>
-      <h2 className='text-[#00FF00] font-bold text-3xl text-center'>
-        Hello World
-      </h2>
-      <textarea className='w-full bg-black outline-none text-[#00FF00] font-medium'></textarea>
+    <section>
+      <Button>Hello World</Button>
     </section>
   );
 }
